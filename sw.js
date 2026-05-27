396510551489a9978929ef1df2509d2d6569b9d9
ishidadev <ryugo1998@live.jp>
2026-05-28 02:13:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'hoikuen-checklist-v1';
 // キャッシュする対象（あなたのHTMLファイル名に書き換えてください）
 const ASSETS = [
   './',
-  './visitns/hoikuen_checklist_app.html' 
+  './hoikuen_checklist_app.html' 
 ];
 
 // インストール時にHTMLをブラウザにガッチリ固定保存
